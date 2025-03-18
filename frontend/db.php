c51c0c9b075e8db76ffa440ceb8ab3aafa0cf44c
Luis Martinez <2002luismm@gmail.com>
2025-03-17 22:16:46 -0500

    Testing the heroku connection

--- frontend/db.php
+++ frontend/db.php
@@ -1,13 +1,9 @@
 <?php
-
-
     // Get Heroku database credentials
-    $url = parse_url(getenv("JAWSDB_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
+    $host = getenv("DB_HOST");
+    $username = getenv("DB_USER");
+    $password = getenv("DB_PASS");
+    $database = getenv("DB_NAME");
 
     // Create connection
     $conn = new mysqli($host, $username, $password, $database);
@@ -16,10 +12,4 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
-    
-    
-    
-
-
 ?>
