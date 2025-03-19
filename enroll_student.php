8114210586f5fa970d1e5686968c8547625b3fd4
Luis Martinez <2002luismm@gmail.com>
2025-03-19 17:50:48 -0500

    Adding enroll student.php file, does not work locally yet

---
+++ enroll_student.php
@@ -0,0 +1,15 @@
+<?php include 'db.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enroll Student in Course</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Enroll Student in Course</h1>
+    
+    <a href="index.php">Back to Home</a>
+</body>
+</html>
