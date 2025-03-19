59675c4676b3c0f7eaa467b87dd8b15755e7d2bb
Luis Martinez <2002luismm@gmail.com>
2025-03-19 16:52:23 -0500

    Added stuff to test locally and on heroku at the same time. Added better visuals for index.php

--- db.php
+++ db.php
@@ -1,14 +1,16 @@
 <?php
-    // Get Heroku database credentials
+
+    require_once 'vendor/autoload.php';
+
+    Dotenv\Dotenv::createImmutable(__DIR__)->load();
+
     $host = getenv("DB_HOST");
     $username = getenv("DB_USER");
     $password = getenv("DB_PASS");
     $database = getenv("DB_NAME");
 
-    // Create connection
     $conn = new mysqli($host, $username, $password, $database);
 
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
