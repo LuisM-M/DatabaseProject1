13c5276c0fcd4b316c5255b80aaa7b6536684b09
Luis Martinez <2002luismm@gmail.com>
2025-03-19 21:08:22 -0500

    Added a quit php file, might change or update later

---
+++ quit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quit</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Goodbye!</h1>
+    <p>You have exited the application.</p>
+</body>
+</html>
