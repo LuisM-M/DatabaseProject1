e4babe221c05787ac66318ef53b47512d07fdbea
LuisM-M <34099456+LuisM-M@users.noreply.github.com>
2025-03-21 10:30:11 -0500

    Update db.php

--- db.php
+++ db.php
@@ -4,16 +4,16 @@
 
     // Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
-    // $host = getenv("DB_HOST");
-    // $username = getenv("DB_USER");
-    // $password = getenv("DB_PASS");
-    // $database = getenv("DB_NAME");
+    $host = getenv("DB_HOST");
+    $username = getenv("DB_USER");
+     $password = getenv("DB_PASS");
+   $database = getenv("DB_NAME");
 
     
-    $host = "localhost";  // From phpMyAdmin
-    $username = "root";  // From phpMyAdmin
-    $password = "";  // From phpMyAdmin
-    $database = "college_db_final";  // From phpMyAdmin
+   // $host = "localhost";  // From phpMyAdmin
+    //$username = "root";  // From phpMyAdmin
+    //$password = "";  // From phpMyAdmin
+    //$database = "college_db_final";  // From phpMyAdmin
 
     $conn = new mysqli($host, $username, $password, $database);
 
